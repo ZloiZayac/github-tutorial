80b0aa543cf7e7c5444c350c8fd465d007c3c354
ZloiZayac <eugenenaydenko@gmail.com>
2020-02-26 23:57:17 +0300

    добавлен новый функционал

--- asd.vhd
+++ asd.vhd
@@ -6,3 +6,15 @@ architecture ruturt_arc of ruturt is
 begin
 
 end ruturt_arc;
+
+sync : process (reset, clock)
+begin
+  if (reset = '1') then
+    
+  elsif (rising_edge(clock)) then
+  	loop_0 : for i in 0 to 10 loop
+  		out <= i;
+  	end loop;
+
+  end if;
+end process sync;
